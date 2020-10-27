0fd92d706940f43e57db9bd1def561d86f7dc5a3
claracheong4 <ccyy4@comp.nus.edu.sg>
2020-10-27 17:22:59 +0800

    Update ShowComponentCommandTest

--- src/test/java/seedu/momentum/logic/commands/ShowComponentCommandTest.java
+++ src/test/java/seedu/momentum/logic/commands/ShowComponentCommandTest.java
@@ -1,6 +1,6 @@
 package seedu.momentum.logic.commands;
 
-import static seedu.momentum.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.momentum.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.momentum.testutil.Assert.assertThrows;
 import static seedu.momentum.testutil.TypicalProjects.getTypicalProjectBook;
 
@@ -12,21 +12,16 @@ import seedu.momentum.model.ModelManager;
 import seedu.momentum.model.UserPrefs;
 
 public class ShowComponentCommandTest {
-
-    private Model model = new ModelManager(getTypicalProjectBook(), new UserPrefs());
-
     @Test
     public void constructor_nullProject_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new ShowComponentCommand(null));
     }
 
     @Test
-    public void execute_dismissReminderComponent_successful() {
+    public void execute_showComponent_throwsCommandException() {
+        Model model = new ModelManager(getTypicalProjectBook(), new UserPrefs());
         ShowComponentCommand showComponentCommand =
                 new ShowComponentCommand(ShowComponentCommandParser.ComponentType.REMINDER);
-        String expectedMessage = String.format(ShowComponentCommand.MESSAGE_SUCCESS,
-                ShowComponentCommandParser.ComponentType.REMINDER.toString(),
-                ShowComponentCommand.REMOVED);
-        assertCommandSuccess(showComponentCommand, model, expectedMessage, model);
+        assertCommandFailure(showComponentCommand, model, ShowComponentCommand.MESSAGE_FAILURE);
     }
 }
