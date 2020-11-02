bf086c7a1612e0d0b3433c8ce4dad8c3cb0db722
pr4aveen <ajjuu98@gmail.com>
2020-11-02 14:35:53 +0800

    Update test util for tasks

--- src/test/java/seedu/momentum/logic/commands/HomeCommandTest.java
+++ src/test/java/seedu/momentum/logic/commands/HomeCommandTest.java
@@ -2,12 +2,12 @@ package seedu.momentum.logic.commands;
 
 import static seedu.momentum.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.momentum.logic.commands.CommandTestUtil.getProjectAtIndex;
+import static seedu.momentum.testutil.TypicalIndexes.INDEX_FIRST_PROJECT;
 import static seedu.momentum.testutil.TypicalProjects.getTypicalProjectBook;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.momentum.commons.core.index.Index;
 import seedu.momentum.model.Model;
 import seedu.momentum.model.ModelManager;
 import seedu.momentum.model.UserPrefs;
@@ -25,7 +25,7 @@ class HomeCommandTest {
 
     @Test
     public void execute_taskView_showsProjectList() {
-        model.viewTasks(getProjectAtIndex(model, Index.fromZeroBased(0)));
+        model.viewTasks(getProjectAtIndex(model, INDEX_FIRST_PROJECT));
         expectedModel.commitToHistory();
         assertCommandSuccess(new HomeCommand(), model, HomeCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT, expectedModel);
     }
