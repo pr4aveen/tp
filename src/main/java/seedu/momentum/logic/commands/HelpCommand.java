84eec683435053211d482499661c6ec5d0d34e09
pr4aveen <ajjuu98@gmail.com>
2020-11-02 11:27:39 +0800

    Modfiy help command to open user guide

--- src/main/java/seedu/momentum/logic/commands/HelpCommand.java
+++ src/main/java/seedu/momentum/logic/commands/HelpCommand.java
@@ -1,5 +1,10 @@
 package seedu.momentum.logic.commands;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import seedu.momentum.model.Model;
 
 /**
@@ -13,9 +18,17 @@ public class HelpCommand extends Command {
             + "Example: " + COMMAND_WORD;
 
     public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_IN_BROWSER = "Opened user guide in default browser.";
 
     @Override
     public CommandResult execute(Model model) {
-        return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        try {
+            Desktop desktop = java.awt.Desktop.getDesktop();
+            URI userGuideUrl = new URI("https://ay2021s1-cs2103t-t10-1.github.io/tp/UserGuide.html");
+            desktop.browse(userGuideUrl);
+            return new CommandResult(SHOWING_HELP_IN_BROWSER, false, false);
+        } catch (URISyntaxException | IOException e) {
+            return new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        }
     }
 }
