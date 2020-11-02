33edb65bc79007900e7fc2b763b6f27160217d22
claracheong4 <ccyy4@comp.nus.edu.sg>
2020-11-02 11:31:06 +0800

    Add tests for AppParameters

--- src/test/java/seedu/momentum/AppParametersTest.java
+++ src/test/java/seedu/momentum/AppParametersTest.java
@@ -1,6 +1,9 @@
 package seedu.momentum;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Paths;
 import java.util.Collections;
@@ -16,25 +19,55 @@ public class AppParametersTest {
 
     private final ParametersStub parametersStub = new ParametersStub();
     private final AppParameters expected = new AppParameters();
+    
+    private static final String CONFIG = "config";
+    private static final String CONFIG_JSON = CONFIG + ".json";
 
     @Test
     public void parse_validConfigPath_success() {
-        parametersStub.namedParameters.put("config", "config.json");
-        expected.setConfigPath(Paths.get("config.json"));
+        parametersStub.namedParameters.put(CONFIG, CONFIG_JSON);
+        expected.setConfigPath(Paths.get(CONFIG_JSON));
         assertEquals(expected, AppParameters.parse(parametersStub));
     }
 
     @Test
     public void parse_nullConfigPath_success() {
-        parametersStub.namedParameters.put("config", null);
+        parametersStub.namedParameters.put(CONFIG, null);
         assertEquals(expected, AppParameters.parse(parametersStub));
     }
 
     @Test
     public void parse_invalidConfigPath_success() {
-        parametersStub.namedParameters.put("config", "a\0");
+        parametersStub.namedParameters.put(CONFIG, "a\0");
         expected.setConfigPath(null);
         assertEquals(expected, AppParameters.parse(parametersStub));
+    }
+
+    @Test
+    public void equals() {
+        // same object -> returns true
+        assertTrue(expected.equals(expected));
+
+        // null -> returns false
+        assertFalse(expected.equals(null));
+
+        // different types -> returns false
+        assertFalse(expected.equals(1));
+    }
+
+    @Test
+    public void hashCodeTest() {
+        expected.setConfigPath(Paths.get(CONFIG_JSON));
+        int expectedHashCode = expected.hashCode();
+        
+        // same values -> returns true
+        AppParameters actual = new AppParameters();
+        actual.setConfigPath(Paths.get(CONFIG_JSON));
+        assertEquals(expectedHashCode, actual.hashCode());
+
+        // different parameters
+        actual.setConfigPath(Paths.get("configs.json"));
+        assertNotEquals(expectedHashCode, actual.hashCode());
     }
 
     private static class ParametersStub extends Application.Parameters {
